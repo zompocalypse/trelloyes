ca02aeb737ca4f0682d1e90b332e394710523b5b
Jason Stankevich <jason.stankevich@gmail.com>
2020-08-24 15:06:06 -0400

    Initial setup and completed card.js

---
+++ src/Card.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Card(props) {
+  return (
+    <div className="Card">
+      <button type="button">delete</button>
+      <h3>{props.title}</h3>
+      <p>{props.content}</p>
+    </div>
+  )
+}
+
+export default Card;
