42733612291b48f0e9e60627792c70a51beac48d
Jason Stankevich <jason.stankevich@gmail.com>
2020-08-24 16:55:20 -0400

    app.js updates

--- src/Card.js
+++ src/Card.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import './Card.css';
+
 function Card(props) {
   return (
-    <div className="Card">
-      <button type="button">delete</button>
+    <div className='Card'>
+      <button type='button'>delete</button>
       <h3>{props.title}</h3>
       <p>{props.content}</p>
     </div>
--- src/list.js
+++ src/list.js
@@ -3,23 +3,28 @@ import Card from './Card';
 import './List.css';
 
 function List(props) {
-    const arrayList = prop.cards.map(card =>
-        <Card key={card.id}
-            title={card.title}
-            content={card.content}/>
-    );
     return (
-        <section className="List">
-            <header className="List-cards">
+        <section className='List'>
+            <header className='List-header'>
                 <h2>{props.header}</h2>
             </header>
-            <div className="List-cards">
-                {arrayList}
-                <button type="button" class="List-add-button">
+            <div className='List-cards'>
+                {props.cards.map((card) =>
+                    <Card 
+                        key={card.id}
+                        title={card.title}
+                        content={card.content}
+                    />
+                )}
+                <button 
+                    type="button" 
+                    className="List-add-button"
+                >
                     + Add Random Card
-          </button>
+                </button>
             </div>
         </section>
     );
 };
 
+export default List; 
